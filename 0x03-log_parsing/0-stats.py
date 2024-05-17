01420e42f5e899a594284affdff7ef26afadbc0b
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-17 19:16:00 +0300

    Update the regex to match a new case of no space between ip and date"

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -31,10 +31,10 @@ def is_valid_line(line):
     <file size>
     """
     pattern = re.compile(
-        r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \['
+        r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|[A-Za-z0-9.-]+)| - \['
         r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+\] '
         r'"GET \/projects\/260 HTTP\/1\.1" '
-        r'(200|301|400|401|403|404|405|500) \d+'
+        r'(?:\d+|[A-Za-z]+) (?:\d+|[A-Za-z]+)'
     )
     return pattern.match(line)
 
@@ -51,24 +51,24 @@ def print_stats():
 
 signal.signal(signal.SIGINT, signal_handler)
 
-
 try:
     for line in sys.stdin:
         try:
             if is_valid_line(line) is False:
                 continue
 
-            line_parts = line.rstrip().split()
+            line_parts = line.split()
             file_size = int(line_parts[-1])
-            status_code = int(line_parts[-2])
-            file_size_accum['total'] += file_size
+            status_code = line_parts[-2]
 
-            if status_code in status_count:
-                status_count[status_code] += 1
+            file_size_accum['total'] += file_size
+            if status_code.isdigit() and int(status_code) in status_count:
+                status_count[int(status_code)] += 1
 
             counter += 1
             if counter % 10 == 0:
                 print_stats()
+
         except (IndexError, ValueError):
             continue
 
