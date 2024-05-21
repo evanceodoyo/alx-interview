b00a33949a6292469037a9a47255ecf82e90e7e3
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-21 23:09:54 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Implements Pascal's Triangle"""
+
+triangle = [[1], [1, 1]]
+
+
+def pascal_triangle(n):
+    """
+    Calculates and returns list of lists of integers representing
+    Pascal's triangle of `n`.
+    """
+    if n <= 0:
+        return []
+
+    if n == 1:
+        return triangle[:1]
+
+    for i in range(1, n - 1):
+        row = [1]
+        for k in range(i):
+            prev = triangle[i][k]
+            nxt = triangle[i][k+1]
+            row.append(prev + nxt)
+            if k == i - 1:
+                row.append(1)
+        triangle.append(row)
+
+    return triangle
